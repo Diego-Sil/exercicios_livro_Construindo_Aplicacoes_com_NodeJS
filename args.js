19df31db44736efa3fa496480dfe6da8fa2a5a8a
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2023-02-25 12:13:55 -0300

    Adicionado exercicio args.js

---
+++ args.js
@@ -0,0 +1,11 @@
+process.argv.forEach(argv =>console.log(argv));
+
+/*
+
+Execute o comando 'node args.js status port 42'
+Resultado
+Na primeira linha é o comando que usamos para executar o programa.
+Na segunda é o caminho para nosso arquivo.
+Nas demais são os argumentos que passamos.
+
+*/
