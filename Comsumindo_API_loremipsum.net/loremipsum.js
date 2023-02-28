6c9be2d01120db78a3f71f0d1796adf705c53a68
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2023-02-28 16:07:29 -0300

    Adicionado boas praticas e validações ao projeto

--- Comsumindo_API_loremipsum.net/loremipsum.js
+++ Comsumindo_API_loremipsum.net/loremipsum.js
@@ -1,16 +1,35 @@
 'use strict';
 
+/*
+loremipsum.js
+
+Faz uma requisição na API `https://dinoipsum.com/api/`
+e grava para um arquivo com o nome e a quantidade 
+de paragrafos
+
+William Bruno, Maio de 2015
+William Bruno, Dezembro de 2020 - Atualizado para es6
+*/
+
 const http = require('https');
 const fs = require('fs')
+const fileName = String(process.argv[2]||'').replace(/[^a-z0-9\.]/gi,'');
+const quantityOfParagraphs = String(process.argv[3]||'').replace(/[^\d]/g,'');
+const USAGE = 'USO: node loremipsum.js{nomeArquivo} {quantidade de paragrafos}';
 
-http.get('https://dinoipsum.com/api/?format=html&words=10&paragraphs=10',res => {
+if(!fileName || !quantityOfParagraphs){
+    return console.log(USAGE);
+}
+
+
+http.get('https://dinoipsum.com/api/?format=html&words=10&paragraphs='+ quantityOfParagraphs,res => {
     let text = '';
     res.on('data',chunk => {
         text += chunk;
     })
     res.on('end',() =>{
-        fs.writeFile('lorem.html',text,()=>{
-            console.log('Arquivo pronto');
+        fs.writeFile(fileName,text,()=>{
+            console.log('Arquivo '+ fileName +' pronto');
         });
     });
 
@@ -20,5 +39,10 @@ http.get('https://dinoipsum.com/api/?format=html&words=10&paragraphs=10',res => 
 });
 
 /* 
-Obs: esse exercicio é feito no livro utilizando a api hhttp://loriipsum.com.br, porem ela estava fora do ar, dai utilizei esta outra API a https://dinoipsum.com que faz a mesma coisa com tematica de dinossauro.
+Obs: no livro utilizado a api hhttp://loriipsum.com.br, porem ela estava fora do ar, dai utilizei esta outra API a https://dinoipsum.com que faz a mesma coisa com tematica de dinossauro.
+Essa api necessita de 3 parametros:
+format - formato do texto que vira, que pode ser html, text ou json(por padrão html)
+words - quantidade de palavras que virar por paragrafo(por padrão 30)
+paragraphs - quantidade de paragrafos(por padrão 10)
+Esses paarametros ja são informados dentro da URL posta na linha 25
 */
